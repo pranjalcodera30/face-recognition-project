839899376ef4aaabd38f336e8a5e8079aef616ca
pranjalcodera30 <pranjaljaypee@gmail.com>
2025-02-21 11:24:12 +0530

    Updated files

--- face_recognition.py
+++ face_recognition.py
@@ -25,6 +25,28 @@ while True:
 
     faces=faceCascade.detectMultiScale(
         gray,
-        scaleFactor
-
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minw),int(minH)),
     )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        id,confidence=recognizer.predict(gray[y:y+h,x:x+w])
+
+        if(confidence<100):
+            id=names[id]
+            confidence=" {0}%".format(round(100-confidence))
+        else:
+            id="unknown"
+            confidence=" {0}%".format(round(100-confidence))
+        
+        cv2.putText(img,str(id),(x+5,y+5),font,1,(255,255,255),2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5),font,1,(255,255,0),1)
+    cv2.imshow('camera',img)
+    k=cv2.waitKey(10) & 0xff
+    if k==27:
+        break
+
+print("\m [INFO] Exisitng programme.")
+cam.release()
+cv2.destroyAllWindows()
