93450a711545d9c5b32c073abd4d37e1d58f196b
pranjalcodera30 <pranjaljaypee@gmail.com>
2025-02-21 10:47:23 +0530

    Updated files

--- training.py
+++ training.py
@@ -8,10 +8,29 @@ recognizer=cv2.face.LBPHFaceRecognizer_create()
 detector=cv2.CascadeClassifier("harrcascade_frontalface_default.xml")
 
 def getImagesAndLabels(path):
-    imagePaths=[os.path.join(path,f) for in os.listdir(path)]
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
     facesamples=[]
     ids=[]
     
 
     for imagePath in imagePaths:
-        PIL_image=Image.open(imagePath) 
+        PIL_image=Image.open(imagePath).convert('L')
+        img_numpy=np.array(PIL_image,'uint8')
+
+        id=int(os.path.split(imagePath)[-1].split(".")[1])
+        faces=detector.detectMultiScale(img_numpy)
+
+        for (x,y,w,h) in faces:
+            facesamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+    return facesamples.ids
+
+print("\n [INFO] Training faces.....")
+faces,ids=getImagesAndLabels(path)
+recognizer.train(faces,np.array(ids))
+recognizer.write('trainer/trainer.yml')
+
+print("\n [INFO] {0} faces trained." .format(len(np.unique(ids))))
+
+
+
